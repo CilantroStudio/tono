b701a9abeb117d764ac9102730006826f4972402
Ratul Maharaj <56479869+RatulMaharaj@users.noreply.github.com>
2024-10-24 03:35:20 +0200

    chore: :bookmark: Bump version

--- src/tono/__init__.py
+++ src/tono/__init__.py
@@ -2,4 +2,4 @@ from tono.agent import Agent  # noqa
 from tono.ai import OpenAICompletionClient, AnthropicCompletionClient  # noqa
 
 __app_name__ = "tono"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
